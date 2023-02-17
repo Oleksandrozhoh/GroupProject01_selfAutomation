1f2793a489fa3ac03ea0eb96e4cad3fa061f0b3e
noor <nooralameri312@gmail.com>
2023-02-16 21:54:28 -0500

    Photo Functionality Other scenarios

--- src/test/java/groupProject/Pages/MeetSkyPhotosPage.java
+++ src/test/java/groupProject/Pages/MeetSkyPhotosPage.java
@@ -11,12 +11,50 @@ public class MeetSkyPhotosPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(xpath = "//li[@data-id='photos'][1]")
-
+    @FindBy(xpath = "//li/a[@aria-label='Photos'][1]")
     public WebElement photo;
 
-
-    @FindBy(xpath = "//div[@class='grid']")
+    @FindBy(id = "hole")
     public WebElement photos;
 
+@FindBy(xpath = "//button[@class='settings-button']")
+public WebElement settingButton;
+
+@FindBy(id = "app-settings__content")
+public WebElement view;
+
+
+@FindBy(xpath = "//button[@class='action-item action-item--single app-navigation-toggle undefined undefined has-tooltip']")
+public WebElement navigationButton;
+
+
+
+
+    @FindBy(xpath = "//span[@title='Your photos']")
+    public WebElement yourPhotos;
+
+    @FindBy(xpath = "//span[@title='Favorites']")
+    public WebElement favorites;
+
+   @FindBy(xpath = "//span[@title='Your videos']")
+    public WebElement videos;
+
+  @FindBy(xpath = "//span[@title='On this day']")
+    public WebElement onThisDay;
+
+
+  @FindBy(xpath = "//span[@title='Your folders']")
+    public WebElement yourFolder;
+
+
+  @FindBy(xpath = "//span[@title='Shared with you']")
+    public WebElement sharedWithYou;
+
+ @FindBy(xpath = "//span[@title='Tagged photos']")
+    public WebElement taggedPhotos;
+
+
+
+
 }
+
--- src/test/java/groupProject/step_definitions/MeetSky_AccessPhotoPage_stepDefinitions.java
+++ src/test/java/groupProject/step_definitions/MeetSky_AccessPhotoPage_stepDefinitions.java
@@ -1,14 +1,15 @@
 package groupProject.step_definitions;
 
 import groupProject.Pages.MeetSkyPhotosPage;
+import groupProject.Utilities.BrowserUtils;
 import groupProject.Utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 
-
 public class MeetSky_AccessPhotoPage_stepDefinitions {
-
+ // Driver.getDriver().get("https://qa.meetsky.net/index.php/login");
+    MeetSkyPhotosPage meetSkyPhotosPage = new MeetSkyPhotosPage();
     @When("user clicks on the  photo .")
     public void user_clicks_on_the_photo() {
         System.out.println("user clicks on photo ");
@@ -16,6 +17,8 @@ public class MeetSky_AccessPhotoPage_stepDefinitions {
         Driver.getDriver().get("https://qa.meetsky.net/index.php/login");
        MeetSkyPhotosPage meetSkyPhotosPage=new MeetSkyPhotosPage();
        meetSkyPhotosPage.photo.click();
+
+
 
 
     }
@@ -28,8 +31,55 @@ public class MeetSky_AccessPhotoPage_stepDefinitions {
         Assert.assertEquals("Title verification!",expectedTitle,actualTitle);
 
 
+
+    }
+    @When("user at photo page.")
+    public void user_at_photo_page() {
+
+        System.out.println("User already on photo page !");
+
+        Driver.getDriver().get("https://qa.meetsky.net/index.php/login");
+        MeetSkyPhotosPage meetSkyPhotosPage=new MeetSkyPhotosPage();
+        meetSkyPhotosPage.photo.click();
+
     }
 
+    @Then("user clicks on the navigation button.")
+    public void user_clicks_on_the_navigation_button() {
+        System.out.println("user clicks on the navigation button.");
+
+        Driver.getDriver().get("https://qa.meetsky.net/index.php/login");
+        MeetSkyPhotosPage meetSkyPhotosPage=new MeetSkyPhotosPage();
+        meetSkyPhotosPage.photo.click();
+        meetSkyPhotosPage.navigationButton.click();
+
+    }
+
+    @Then("user can see photo view")
+    public void userCanSeePhotoView() {
+
+        System.out.println("photo view verification");
+        meetSkyPhotosPage.photo.click();
+        meetSkyPhotosPage.navigationButton.click();
+       meetSkyPhotosPage.view.getText();
+
+
+    }
+
+    @Then("user can see checkbox under settings")
+    public void userCanSeeCheckboxUnderSettings() {
+        System.out.println("checkBox exist verification!");
+
+        meetSkyPhotosPage.settingButton.click();
+
+
+
+
+
+       BrowserUtils.logout();
+    }
 }
+
+
 
 
